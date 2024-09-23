6dee2884add6c9b4e1941cbf1992c869e42ae35e
Robert <rmjesusaddict@gmail.com>
2024-09-23 14:10:10 +0300

    Make the first model for users

---
+++ backend/models/user.js
@@ -0,0 +1,93 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+const sequelize = require('../config/sequelize');
+
+class User extends Model {
+    // Define instance method for the class
+    async recentPosts() {
+        return await this.getposts({
+            order: [['createdAt', 'DESC']],
+            limit: 5
+        })
+    }
+
+}
+
+User.init({
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: {
+                msg: 'Please enter a valid email'
+            },
+            notEmpty: {
+                msg: 'Please enter email to proceed.'
+            }
+        }
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'Please enter your name',
+            },
+            len: {
+                args: [0, 100],
+                msg: 'Your name cannot exceed 100 characters'
+            }
+        }
+    },
+    photo: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    Bio: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'Please enter your name',
+            },
+            len: {
+                args: [0, 500],
+                msg: 'Your bio cannot exceed 500 characters'
+            }
+        }},
+    Password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: 'Please enter your password'
+            },
+            len: {
+                args: [8, undefined],
+                msg: 'Your password must be longer than 8 characters'
+            }
+        }
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    }
+});
+
+// Encrypting password before update user
+User.beforeSave(async (user, options) => {
+    if (user.changed('password')) {
+      user.password = await bcrypt.hash(user.password, 10);
+    }
+  });
+
+  //Associations
+  User.associate = (models) => {
+      User.hasMany(models.Comment, {foreignKey: 'user_id'});
+      User.hasMany(models.Post, {foreignKey: 'author_id', sourceKey: 'id'});
+      User.hasMany(models.Like, {foreignKey: 'user_id'});
+  }
+
+
+  module.exports= User;
