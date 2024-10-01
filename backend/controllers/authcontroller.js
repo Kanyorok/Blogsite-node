5e9ce1ce7187607a5be479770241e9afc80f7f9d
Robert <rmjesusaddict@gmail.com>
2024-10-01 20:12:20 +0300

    Add logic for sign in user

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -1,6 +1,8 @@
 const User = require('../models/user');
 const catchAsyncError = require('../middleware/catchasyncErrors');
 const sendToken = require('../utils/jwtTokens');
+const ErrorHandler = require('../utils/errorHandler');
+
 // Register user => /api/v1/register
 exports.registerUser = catchAsyncError (async(req, res, next) => {
     
@@ -16,4 +18,32 @@ exports.registerUser = catchAsyncError (async(req, res, next) => {
 
     sendToken(user, 200, res);
     
+});
+
+// Login user => /api/v1/login
+exports.loginUser = catchAsyncError(async (req, res, next)=> {
+    const {email, password} = req.body;
+
+    // check if email and password are entered by user
+    if (!email || !password){
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+    }
+
+    // Find user in db
+    const user = await User.findOne({
+        where: {email},
+        attributes: {include: ['password']}
+    });
+
+    if (!user) {
+        return next(new ErrorHandler('invalid Email or Password', 401));
+    }
+    // check if password provided is correct or not
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler('invalid Email or Password', 401));
+    }
+
+    sendToken(user, 200, res);
 })
