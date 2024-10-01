af7d7bc31e239326aae2422cd4c97f0e5d36ee6c
Robert <rmjesusaddict@gmail.com>
2024-10-01 20:14:12 +0300

    Add the api end point to login a user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser } = require('../controllers/authcontroller');
+const { registerUser, loginUser } = require('../controllers/authcontroller');
 
 router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
 
 
 module.exports = router;
